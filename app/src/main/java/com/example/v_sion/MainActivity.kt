10b57c5f88735f64a88b5c57451ef3f1d7849519
Teek Leng Ng <jeff2406n@gmail.com>
2021-04-14 16:09:44 +0100

    handle up navigation in actionbarbar/toolbar and added logout fragment

--- app/src/main/java/com/example/v_sion/MainActivity.kt
+++ app/src/main/java/com/example/v_sion/MainActivity.kt
@@ -2,17 +2,19 @@ package com.example.v_sion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.NavHostFragment.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,27 +23,50 @@ class MainActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        // Bottom Navigation
-        setupBottomNavMenu()
-    }
-
-    private fun setupBottomNavMenu() {
         // Finding the Navigation Controller
         var navController = findNavController(R.id.navHostFragment)
 
-        bottom_nav_view.setupWithNavController(navController)
-
         // Setting Up ActionBar with Navigation Controller
         // Pass the IDs of top-level destinations in AppBarConfiguration
-        var appBarConfiguration = AppBarConfiguration(
-            topLevelDestinationIds = setOf (
+        appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(
                 R.id.targetAppFragment,
                 R.id.homeFragment,
                 R.id.focusLockFragment
             )
         )
-        // change toolbar label
+        // Bottom Navigation
+        setupBottomNavMenu(navController)
+        setupActionBar(navController)
+
+
+    }
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        // change toolbar label (Target App, Home, Focus Lock)
+        bottom_nav_view.setupWithNavController(navController)
+    }
+
+    private fun setupActionBar(navController: NavController) {
+        // Display the Up button whenever you're not on a top-level destination
         setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.overflow_menu, menu)
+        return true
+    }
+
+    // Handle navigation with onNavDestinationSelected helper method,
+    // if the menu item is not meant to navigate, handel with super.onOptionsItemSelected
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(findNavController(R.id.navHostFragment))
+                || super.onOptionsItemSelected(item)
+    }
+
+    // Handle up navigation behavior in the ActionBar
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.navHostFragment).navigateUp(appBarConfiguration)
     }
 
 
